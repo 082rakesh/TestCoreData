648647c34b8b97a64de7f7d323512bb0751440bf
082rakesh <082rakesh@gmail.com>
2018-07-25 15:51:36 +0530

    Initial Commit

---
+++ TestCoreData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestCoreData
+//
+//  Created by r.f.kumar.mishra on 25/07/18.
+//  Copyright © 2018 r.f.kumar.mishra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
